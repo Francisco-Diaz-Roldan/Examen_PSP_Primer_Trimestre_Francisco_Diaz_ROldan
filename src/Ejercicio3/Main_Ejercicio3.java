b0cf6fff44e097312e8835c4e94d33b5639e48b4
Francisco-Diaz-Roldan <pacodiaz008@gmail.com>
2023-12-18 11:13:22 +0100

    Ejercicio1 completado. Ejercicio3 completado.

--- src/Ejercicio3/Main_Ejercicio3.java
+++ src/Ejercicio3/Main_Ejercicio3.java
@@ -12,20 +12,17 @@ public class Main_Ejercicio3 {
     entrada un array de tamaño aleatorio de números enteros (entre el 1 y el 100),
     y que nos devuelva un array con los números pares de ese array ordenados de
     menor a mayor.*/
+         List<Integer> lista = new ArrayList<>();
+         Nodo.addRandom(new Random(), lista);
 
-        public static void main(String[] args) throws InterruptedException {
-            List<Integer> lista = new ArrayList<>();
-            Nodo.addRandom(new Random(), lista);
-
-            Nodo nodo = new Nodo(lista);
-            nodo.start();
-            System.out.println("Array generado de forma aleatoria:");
-            Nodo.mostrarLista(nodo.getLista());
-            nodo.join();
-            System.out.println();
-
-            System.out.println("Array de números pares ordenados de menor a mayor:");
-            Nodo.mostrarListaPares(nodo.getLista());
+         Nodo nodo = new Nodo(lista);
+         nodo.start();
+         System.out.println("Array generado de forma aleatoria:");
+         Nodo.mostrarLista(nodo.getLista());
+         nodo.join();
+         System.out.println();
+         System.out.println("Array de números pares ordenados de menor a mayor:");
+         Nodo.mostrarListaPares(nodo.getLista());
         }
     }
 }
